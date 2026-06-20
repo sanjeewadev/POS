d7253f1b3afc9332288547f2c75d60d3457925c1
sanjeewa-dev <svk6061@gmail.com>
2026-06-20 11:23:36 +0530

    Almost Over 3

---
+++ POS.BackOffice.UI/Views/Pages/Reports/FinancialSummaryView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace POS.BackOffice.UI.Views.Pages.Reports
+{
+    public partial class FinancialSummaryView : UserControl
+    {
+        public FinancialSummaryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- POS.BackOffice.UI/obj/Debug/net8.0-windows/Views/Pages/Sales/FinancialSummaryView.g.i.cs
+++ POS.BackOffice.UI/obj/Debug/net8.0-windows/Views/Pages/Sales/FinancialSummaryView.g.i.cs
@@ -53,7 +53,8 @@ namespace POS.BackOffice.UI.Views.Pages.Sales {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/POS.BackOffice.UI;component/views/pages/sales/financialsummaryview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/POS.BackOffice.UI;V1.0.0.0;component/views/pages/sales/financialsummaryview.xaml" +
+                    "", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\Views\Pages\Sales\FinancialSummaryView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
